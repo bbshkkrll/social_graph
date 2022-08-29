5a6fff853870ed244d0d23fc23f0c511bfd3ac9a
Kirill Bobeshko <bobeshko@icloud.com>
2022-08-29 20:14:51 +0500

    [upd] new project structure and flask startup

--- social_graph/app.py
+++ social_graph/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('force_diricted_graph.html')
 
 
 if __name__ == '__main__':
