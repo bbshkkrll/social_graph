3be01519b7a6d7ca8a5bc154946958668164b39e
Kirill Bobeshko <bobeshko@icloud.com>
2022-09-06 21:45:27 +0500

    to heroku

--- social_graph/modules/auth.py
+++ social_graph/modules/auth.py
@@ -1,7 +1,9 @@
-print(
-    """https://oauth.vk.com/authorize?client_id=51395060&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=offline,friends&response_type=token&v=5.131""")
-'vk1.a.vC0oE028Yb611NTWQFQc0Fk02Dx5AUKGJeFVxr-ZkOicsEI-AfZqSjI2IdO2uCiaHs28LXsSpYuM1QZTpVDeI56OkfpD-jtJOdxkd4_bAYaFA-ZY6foL1lvbcXziraAXTSMcB_W5Vw7XXi4awC7ACjn2RWpcSVPjZZjuDDwznr6lS39bNZCkxRplXWMSpjkz'
+import os
 
-a = [{'1': '1'}, {'2': '2'}]
-b = [{'1': '1'}]
-print(list(set(a) - set(b)))
+from vk_api import VkApi
+
+user = VkApi(login='89512517109', password='P0lina2025', token=os.environ['VK_API_TOKEN'])
+
+user.auth()
+
+
