a10aaa84827a0f368aca1275323a9d85f37dfc05
Kirill Bobeshko <bobeshko@icloud.com>
2022-09-18 00:47:45 +0500

    update app.py

--- social_graph/app.py
+++ social_graph/app.py
@@ -32,24 +32,11 @@ def auth():
     response.set_cookie('token', token)
     response.set_cookie('current_id', current_id)
 
-    session.modified = True
-
     return response
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        print(request.form['uid'])
-        main_id = request.form['uid']
-        vk_session = VkSession()
-        try:
-
-            user_info = UserData(main_id, *vk_session.prepare_data(main_id))
-            user_info.initialize_friends()
-            user_info.dump_data_to_json('./static/data/graph_data.json')
-        except VkException as e:
-            print(e.message, e.code)
     return render_template('login.html')
 
 
