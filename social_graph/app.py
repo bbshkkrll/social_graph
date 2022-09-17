be488df21999ae1d4497890d6efd271929f7c584
Kirill Bobeshko <bobeshko@icloud.com>
2022-09-18 01:33:16 +0500

    bugs fixed in app.py

--- social_graph/app.py
+++ social_graph/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 
 @app.route("/graph")
 def graph():
-    token = request.cookies.get('token')
+    code = request.cookies.get('code')
+    vk_session = VkSession()
+    token = vk_session.get_access_token(code)
     vk_session = VkSession(token=token)
     try:
-        user_info = UserData(request.cookies.get('current_id'), *vk_session.prepare_data())
+        user_info = UserData(vk_session.get_current_user_id(), *vk_session.prepare_data())
         user_info.initialize_friends()
         user_info.dump_data_to_json('./static/data/graph_data.json')
     except VkException as e:
@@ -23,15 +25,9 @@ def graph():
 def auth():
     code = request.args.get('code')
 
-    vk_session = VkSession()
-    token = vk_session.get_access_token(code)
-    vk_session = VkSession(token=token)
-    current_id = vk_session.get_current_user_id()
     response = make_response(
-        render_template('login.html', token=token, user_info=current_id))
+        render_template('login.html'))
     response.set_cookie('code', code)
-    response.set_cookie('token', token)
-    response.set_cookie('current_id', current_id)
 
     return response
 
