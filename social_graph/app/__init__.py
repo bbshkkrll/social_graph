6b5d9dd9405a33d059acc0655425c089bfbf84e3
Kirill Bobeshko <bobeshko@icloud.com>
2022-10-25 01:16:25 +0500

    full db url

--- social_graph/app/__init__.py
+++ social_graph/app/__init__.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 
 app_session = VkSession()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_MAROON_URL'].replace('postgres', 'postgresql')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kdrdfagntluhbd:400c53c53e9e1f0e6f082c609f65929845bbd183f59f359ce22368bee43b58eb@ec2-52-23-131-232.compute-1.amazonaws.com:5432/d4ec5vo65efgag'
 db = SQLAlchemy(app)
