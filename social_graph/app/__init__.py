657e51d241aa54432191d70e563fd9f3948a3354
Kirill Bobeshko <bobeshko@icloud.com>
2022-10-26 19:30:19 +0500

    создание сессии базы данных отдельно (добавил создание движка)

--- social_graph/app/__init__.py
+++ social_graph/app/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from flask import Flask
 
 from social_graph.app.vk_session import VkSession
@@ -12,6 +13,7 @@ app_session = VkSession()
 
 app.config[
     'SQLALCHEMY_DATABASE_URI'] = 'postgresql://fonhqoavpqasjt:226cfdbe5427c0d6d5e346ddbda2f2f49b785d3ff80b95d997a0684daa8cbeb7@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dn0gdbcvibf1t'
-db = SQLAlchemy(app)
 
-db_session = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=engine)
+db_session = Session()
