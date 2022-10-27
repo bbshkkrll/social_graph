0c3f7f4fb507ae9145c2d0dfd23db7e20055b57b
Kirill Bobeshko <bobeshko@icloud.com>
2022-10-27 11:02:18 +0500

    изменил конфиг приложения

--- social_graph/app/__init__.py
+++ social_graph/app/__init__.py
@@ -11,8 +11,11 @@ app = Flask(__name__)
 
 app_session = VkSession()
 
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://fonhqoavpqasjt:226cfdbe5427c0d6d5e346ddbda2f2f49b785d3ff80b95d997a0684daa8cbeb7@ec2-44-199-9-102.compute-1.amazonaws.com:5432/dn0gdbcvibf1t'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fonhqoavpqasjt:226cfdbe5427c0d6d5e346ddbda2f2f49b785d3ff8' \
+                                        '0b95d997a0684daa8cbeb7@ec2-44-199-9-102.compu' \
+                                        'te-1.amazonaws.com:5432/dn0gdbcvibf1t'
+
+app.config['JSON_AS_ASCII'] = False
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 Session = sessionmaker(bind=engine)
