9e2d945d1a77f39cb052986162f637905cf7fa40
bbshkkrll <bobeshko@icloud.com>
2022-10-31 18:18:11 +0500

    изменил локигу обработки запросов и работу с базой данных

--- run.py
+++ run.py
@@ -1,30 +1,40 @@
-import json
-
-from flask import request, render_template, make_response, redirect, url_for, jsonify
+from flask import request, render_template, make_response, redirect, url_for, jsonify, session
 
 from app import app, app_session, db_session
 from app.models import Token, User, Graph
 from app.modules.vk_exception import VkException
 
+from sqlalchemy.exc import NoResultFound
+
 
 @app.route("/graph")
 def graph():
+    if 'vk_user_id' in session:
+        return render_template('index.html')
     try:
-        if 'is_auth' in request.cookies.keys():
-            return render_template('index.html')
-    except Exception as e:
-        print(e.args)
-    try:
-        code = request.cookies.get('code')
+        code = session['code']
         access = app_session.get_access(code)
         token = Token(access['user_id'], access['access_token'], access['expires_in'])
         usr = User(token)
 
-        graph = usr.get_graph()
+        session['vk_user_id'] = usr.vk_user_id
 
+        try:
+            old_user = db_session.query(User).filter(User.vk_user_id == usr.vk_user_id).one()
+            old_graph = db_session.query(Graph).filter(Graph.id == old_user.graph_id).one()
+            old_token = db_session.query(Token).filter(Token.id == old_user.token_id)
+
+            with app.app_context():
+                db_session.delete(old_user)
+                db_session.delete(old_graph)
+                db_session.delete(old_token)
+                db_session.commit()
+        except NoResultFound:
+            pass
+
+        graph = usr.get_graph()
         response = make_response(render_template('index.html'))
-        response.set_cookie('usr_id', str(usr.vk_user_id))
-        response.set_cookie('is_auth', '1')
+        session['vk_user_id'] = str(usr.vk_user_id)
         with app.app_context():
             db_session.add(token)
             db_session.add(graph)
@@ -40,7 +50,7 @@ def graph():
 def auth():
     response = make_response(redirect(url_for('graph')))
     code = request.args.get('code')
-    response.set_cookie('code', code)
+    session['code'] = code
 
     return response
 
@@ -52,11 +62,17 @@ def login():
 
 @app.route('/data')
 def send_data():
+    if 'vk_user_id' not in session:
+        return make_response(redirect(url_for('/')))
+
+    vk_user_id = session['vk_user_id']
     with app.app_context():
-        vk_user_id = request.cookies.get('usr_id')
-        user = db_session.query(User).filter(User.vk_user_id == vk_user_id).one()
-        data = db_session.query(Graph).filter(Graph.id == user.graph_id).one().data
-        return jsonify(data)
+        try:
+            user = db_session.query(User).filter(User.vk_user_id == vk_user_id).one()
+            data = db_session.query(Graph).filter(Graph.id == user.graph_id).one().data
+            return jsonify(data)
+        except NoResultFound as e:
+            return make_response(render_template(url_for('/')))
 
 
 if __name__ == '__main__':
